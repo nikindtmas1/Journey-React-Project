d5685f6b1113baaa5cf12aeb032c89649de0651d
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-21 21:24:49 +0200

    Create top destination

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 
 import Home from "./components/Home/Home";
 import Navigation from "./components/Navigation/Navigation";
+import Destination from "./components/TopDestination/TopDestination";
 
 function App() {
   return (
@@ -10,48 +11,7 @@ function App() {
 
         <div class="tm-page-wrap mx-auto">
             <Home />
-            
-            <div class="tm-container-outer" id="tm-section-2">
-                <section class="tm-slideshow-section">
-                    <div class="tm-slideshow">
-                        <img src="img/tm-img-01.jpg" alt="Image" />
-                        <img src="img/tm-img-02.jpg" alt="Image" />
-                        <img src="img/tm-img-03.jpg" alt="Image" />    
-                    </div>
-                    <div class="tm-slideshow-description tm-bg-primary">
-                        <h2 class="">Europe's most visited places</h2>
-                        <p>Aenean in lacus nec dolor fermentum congue. Maecenas ut velit pharetra, pharetra tortor sit amet, vulputate sem. Vestibulum mi nibh, faucibus ac eros id, sagittis tincidunt velit. Proin interdum ullamcorper faucibus. Ut mi nunc, sollicitudin non pulvinar id, sagittis eget diam.</p>
-                        <a href="#" class="text-uppercase tm-btn tm-btn-white tm-btn-white-primary">Continue Reading</a>
-                    </div>
-                </section>
-                <section class="clearfix tm-slideshow-section tm-slideshow-section-reverse">
-
-                    <div class="tm-right tm-slideshow tm-slideshow-highlight">
-                        <img src="img/tm-img-02.jpg" alt="Image" />
-                        <img src="img/tm-img-03.jpg" alt="Image" />
-                        <img src="img/tm-img-01.jpg" alt="Image" />
-                    </div> 
-
-                    <div class="tm-slideshow-description tm-slideshow-description-left tm-bg-highlight">
-                        <h2 class="">Asia's most popular places</h2>
-                        <p>Vivamus in massa ullamcorper nunc auctor accumsan ac at arcu. Donec sagittis mattis pharetra. Proin commodo, ante et volutpat pulvinar, arcu arcu ullamcorper diam, id maximus sem tellus id sem. Suspendisse eget rhoncus diam. Fusce urna elit, porta nec ullamcorper id.</p>
-                        <a href="#" class="text-uppercase tm-btn tm-btn-white tm-btn-white-highlight">Continue Reading</a>
-                    </div>                        
-
-                </section>
-                <section class="tm-slideshow-section">
-                    <div class="tm-slideshow">
-                        <img src="img/tm-img-03.jpg" alt="Image" />
-                        <img src="img/tm-img-02.jpg" alt="Image" />
-                        <img src="img/tm-img-01.jpg" alt="Image" />
-                    </div>
-                    <div class="tm-slideshow-description tm-bg-primary">
-                        <h2 class="">America's most famous places</h2>
-                        <p>Donec nec laoreet diam, at vehicula ante. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse nec dapibus nunc, quis viverra metus. Morbi eget diam gravida, euismod magna vel, tempor urna.</p>
-                        <a href="#" class="text-uppercase tm-btn tm-btn-white tm-btn-white-primary">Continue Reading</a>
-                    </div>
-                </section>
-            </div>        
+           <Destination />       
             <div class="tm-container-outer" id="tm-section-3">
                 <ul class="nav nav-pills tm-tabs-links">
                     <li class="tm-tab-link-li">
