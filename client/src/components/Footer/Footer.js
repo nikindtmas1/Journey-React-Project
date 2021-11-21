a9d98af48178ebcfe361b06c14a0431c189c8a6d
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-21 21:36:43 +0200

    Create Footer.js

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+    return (
+        <footer className="tm-container-outer">
+        <p className="mb-0">Copyright © <span className="tm-current-year">2018</span> Your Company 
+            
+        . Designed by <a rel="nofollow" href="http://www.google.com/+templatemo" target="_parent">Template Mo</a></p>
+    </footer>
+    );
+};
+
+export default Footer;
