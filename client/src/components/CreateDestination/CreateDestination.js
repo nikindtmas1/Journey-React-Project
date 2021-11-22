d9c70f20c62c90bafd6acc6ae14ec5bd2cd17c5e
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-22 22:05:35 +0200

    Change img name

--- client/src/components/CreateDestination/CreateDestination.js
+++ client/src/components/CreateDestination/CreateDestination.js
@@ -25,13 +25,13 @@ const CreateDestination = () => {
                         <textarea id="contact_message" name="contact_message" className="form-control" rows="5" placeholder="Message" required></textarea>
                     </div> */}
                     <div className="form-group">
-                        <input type="text" id="contact_subject" name="img-01" className="form-control" placeholder="Image One"  required/>
+                        <input type="text" id="contact_subject" name="imgOne" className="form-control" placeholder="Image One"  required/>
                     </div>
                     <div className="form-group">
-                        <input type="text" id="contact_subject" name="img-02" className="form-control" placeholder="Image Two"  required/>
+                        <input type="text" id="contact_subject" name="imgTwo" className="form-control" placeholder="Image Two"  required/>
                     </div>
                     <div className="form-group">
-                        <input type="text" id="contact_subject" name="img-03" className="form-control" placeholder="Image Three"  required/>
+                        <input type="text" id="contact_subject" name="imgThree" className="form-control" placeholder="Image Three"  required/>
                     </div>
                    
                     <button type="submit" className="btn btn-primary tm-btn-primary tm-btn-send text-uppercase">Add Destination Now</button>
