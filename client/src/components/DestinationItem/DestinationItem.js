82429b2a5ee2a34cb8f051ff466d45037f36412d
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-22 22:09:31 +0200

    Create description item

--- client/src/components/DestinationItem/DestinationItem.js
+++ client/src/components/DestinationItem/DestinationItem.js
@@ -1,21 +1,17 @@
 
 const DestinationItem = ({
-    name,
-    description,
-    imgOne,
-    imgTwo,
-    imgThree
+    destination 
 }) => {
     return (
         <section className="tm-slideshow-section">
         <div className="tm-slideshow">
-            <img src="img/tm-img-01.jpg" alt="Image" />
-            <img src="img/tm-img-02.jpg" alt="Image" />
-            <img src="img/tm-img-03.jpg" alt="Image" />    
+            <img src={destination.imgOne} alt="Image" />
+            <img src={destination.imgTwo} alt="Image" />
+            <img src={destination.imgThree} alt="Image" />    
         </div>
         <div className="tm-slideshow-description tm-bg-primary">
-            <h2 className="">Europe's most visited places</h2>
-            <p>Aenean in lacus nec dolor fermentum congue. Maecenas ut velit pharetra, pharetra tortor sit amet, vulputate sem. Vestibulum mi nibh, faucibus ac eros id, sagittis tincidunt velit. Proin interdum ullamcorper faucibus. Ut mi nunc, sollicitudin non pulvinar id, sagittis eget diam.</p>
+            <h2 className="">{destination.name}</h2>
+            <p>{destination.description}</p>
             <a href="#" className="text-uppercase tm-btn tm-btn-white tm-btn-white-primary">Continue Reading</a>
         </div>
     </section>
