18b0fc310604d51e7ab80b9159fc165538f7d2e7
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-23 14:36:38 +0200

    Create new component

---
+++ client/src/components/Places/Container/Fourth.js
@@ -0,0 +1,66 @@
+
+const FourtPlace = () => {
+    return (
+        <div className="tab-pane fade show active" id="4a">
+
+        <div className="tm-recommended-place-wrap">
+          <div className="tm-recommended-place">
+            <img src="img/tm-img-06.jpg" alt="Image" className="img-fluid tm-recommended-img" />
+            <div className="tm-recommended-description-box">
+              <h3 className="tm-recommended-title">Asia Resort Hotel</h3>
+              <p className="tm-text-highlight">Singapore</p>
+              <p className="tm-text-gray">Sed egestas, odio nec bibendum mattis, quam odio hendrerit risus, eu varius eros lacus sit amet lectus. Donec blandit luctus dictum...</p>
+            </div>
+            <a href="#" className="tm-recommended-price-box">
+              <p className="tm-recommended-price">$440</p>
+              <p className="tm-recommended-price-link">Continue Reading</p>
+            </a>
+          </div>
+
+          <div className="tm-recommended-place">
+            <img src="img/tm-img-07.jpg" alt="Image" className="img-fluid tm-recommended-img" />
+            <div className="tm-recommended-description-box">
+              <h3 className="tm-recommended-title">Nullam eget est a nisl</h3>
+              <p className="tm-text-highlight">Yangon, Myanmar</p>
+              <p className="tm-text-gray">Sed egestas, odio nec bibendum mattis, quam odio hendrerit risus, eu varius eros lacus sit amet lectus. Donec blandit luctus dictum...</p>
+            </div>
+            <div id="preload-hover-img"></div>
+            <a href="#" className="tm-recommended-price-box">
+              <p className="tm-recommended-price">$450</p>
+              <p className="tm-recommended-price-link">Continue Reading</p>
+            </a>
+          </div>
+
+          <div className="tm-recommended-place">
+            <img src="img/tm-img-05.jpg" alt="Image" className="img-fluid tm-recommended-img" />
+            <div className="tm-recommended-description-box">
+              <h3 className="tm-recommended-title">Proin interdum ullamcorper</h3>
+              <p className="tm-text-highlight">Bangkok, Thailand</p>
+              <p className="tm-text-gray">Sed egestas, odio nec bibendum mattis, quam odio hendrerit risus, eu varius eros lacus sit amet lectus. Donec blandit luctus dictum...</p>
+            </div>
+            <a href="#" className="tm-recommended-price-box">
+              <p className="tm-recommended-price">$460</p>
+              <p className="tm-recommended-price-link">Continue Reading</p>
+            </a>
+          </div>
+
+          <div className="tm-recommended-place">
+            <img src="img/tm-img-04.jpg" alt="Image" className="img-fluid tm-recommended-img" />
+            <div className="tm-recommended-description-box">
+              <h3 className="tm-recommended-title">Lorem ipsum dolor sit</h3>
+              <p className="tm-text-highlight">Vientiane, Laos</p>
+              <p className="tm-text-gray">Sed egestas, odio nec bibendum mattis, quam odio hendrerit risus, eu varius eros lacus sit amet lectus. Donec blandit luctus dictum...</p>
+            </div>
+            <a href="#" className="tm-recommended-price-box">
+              <p className="tm-recommended-price">$470</p>
+              <p className="tm-recommended-price-link">Continue Reading</p>
+            </a>
+          </div>
+        </div>
+
+        <a href="#" className="text-uppercase btn-primary tm-btn mx-auto tm-d-table">Show More Places</a>
+      </div>
+    );
+};
+
+export default FourtPlace;
