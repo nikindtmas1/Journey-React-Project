b95475dddabcad38dc76a1f9a26e1fcd352caa9d
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-23 21:45:06 +0200

    Change rest server

--- client/src/components/Services/data.js
+++ client/src/components/Services/data.js
@@ -1,15 +1,16 @@
 import * as service from '../Services/destinationService';
 
-const host = 'http://localhost:3030';
-//const host = 'http://localhost:5000';
+//const host = 'http://localhost:3030';
+const host = 'http://localhost:5000';
 service.settings.host = host;
 
 export async function getAll(){
-    let destinations = await service.get(host + '/jsonstore/destinations');
+    let destinations = await service.get(host + '/journey/destinations');
     let result = Object.values(destinations);
     return result;
 };
 
 export async function create(data){
-    return await service.post(host + '/jsonstore/destinations', data);
+    return await service.post(host + '/journey/destinations', data);
 };
+//return await service.post(host + '/jsonstore/destinations', data);
