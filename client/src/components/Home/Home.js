9fecd4b6b5956752591955be01e0d81f0fba5c6e
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-24 15:28:17 +0200

    Added Link to

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
@@ -12,7 +13,7 @@ const Home = () => {
                                     <h1 className="text-uppercase tm-banner-title">Let's begin</h1>
                                     <img src="img/dots-3.png" alt="Dots" />
                                     <p className="tm-banner-subtitle">We assist you to choose the best.</p>
-                                    <a href="javascript:void(0)" className="tm-down-arrow-link"><i className="fa fa-2x fa-angle-down tm-down-arrow"></i></a>       
+                                    <Link to="/destination" className="tm-down-arrow-link"><i className="fa fa-2x fa-angle-down tm-down-arrow"></i></Link>       
                                 </div>    
                             </div>                        
                         </div> 
@@ -102,7 +103,7 @@ const Home = () => {
                         <div className="col-xs-12 mx-auto tm-about-text-wrap text-center">                        
                             <h2 className="text-uppercase mb-4">Your <strong>Journey</strong> is our priority</h2>
                             <p className="mb-4">Nullam auctor, sapien sit amet lacinia euismod, lorem magna lobortis massa, in tincidunt mi metus quis lectus. Duis nec lobortis velit. Vivamus id magna vulputate, tempor ante eget, tempus augue. Maecenas ultricies neque magna.</p>
-                            <a href="/places" className="text-uppercase btn-primary tm-btn">Continue explore</a>                              
+                            <Link to="/places" className="text-uppercase btn-primary tm-btn">Continue explore</Link>                              
                         </div>
                     </div>
                 </div>            
