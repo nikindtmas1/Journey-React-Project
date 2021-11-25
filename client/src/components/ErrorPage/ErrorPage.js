b201be54829737ff65378698914c87b13695da06
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-25 14:53:42 +0200

    Added link to error page

--- client/src/components/ErrorPage/ErrorPage.js
+++ client/src/components/ErrorPage/ErrorPage.js
@@ -1,8 +1,9 @@
-
+import { Link } from 'react-router-dom';
 const ErrorPage = () => {
     return (
         <div >
             <h1>Error Page</h1>
+            <Link to='/'><h3>Go to home page</h3></Link>
         </div>
     );
 };
