ff36fd5f19141eb594752197ca23e3e3adab0aff
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-26 14:48:45 +0200

    Change path

--- client/src/components/Services/placesData.js
+++ client/src/components/Services/placesData.js
@@ -2,31 +2,31 @@ import * as placeServices from '../Services/destinationService';
 
 //const host = 'http://localhost:3030';
 const host = 'http://localhost:5000';
-service.settings.host = host;
-const collection = '/journey/places';
+placeServices.settings.host = host;
+
 
 
 export async function getAll(endUrl){
-    let places = await placeServices.get(host + collection + endUrl);
+    let places = await placeServices.get(host +  `/journey${endUrl}`);
     let result = Object.values(places);
     return result;
 };
 
 export async function create(data, endUrl){
-    return await placeServices.post(host + collection + endUrl, data);
+    return await placeServices.post(host +  `/journey/${endUrl}`, data);
 };
 //return await service.post(host + '/jsonstore/destinations', data);
 
-export async function getOne(id){
-    let result = await placeServices.get(host + `${collection}/${endUrl}/${id}`);
+export async function getOne(id, endUrl){
+    let result = await placeServices.get(host + `/journey/${endUrl}/${id}`);
     return result;
 };
 
-export async function edit(id, data){
-    const response = await placeServices.put(host + `${collection}/${endUrl}/${id}`, data);
+export async function edit(id,endUrl, data){
+    const response = await placeServices.put(host + `/journey/${endUrl}/${id}`, data);
     return response
 }
 
-export async function deleteDestination(id){
-    return await placeServices.del(host + `${collection}/${endUrl}/${id}`);
+export async function deleteDestination(id, endUrl){
+    return await placeServices.del(host + `/journey/${endUrl}/${id}`);
 }
