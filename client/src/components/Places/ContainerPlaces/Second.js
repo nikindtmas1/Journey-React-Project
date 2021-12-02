6da7f7bd24d7dd1216395d96a18a0ab6c3ca973b
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-12-02 11:31:28 +0200

    Added loggedIn user logic for create button

--- client/src/components/Places/ContainerPlaces/Second.js
+++ client/src/components/Places/ContainerPlaces/Second.js
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { Link ,useRouteMatch } from 'react-router-dom';
 
+import AuthCxt from '../../../contexts/AuthCxt';
 import PlaceItem from '../PlaceItem/PlaceItem';
 import * as southAmericaService from '../../Services/southAmericaData';
 
@@ -9,17 +10,26 @@ const SecondPlace = () => {
 
   const [places, setPlaces] = useState([]);
   let path = useRouteMatch();
+  const value = useContext(AuthCxt);
+  let user = value.user.user;
 
   useEffect(() => {
       southAmericaService.getAll()
       .then(result => setPlaces(result))
   }, []);
+
+  let loggedInUser = (
+    <Link to={`/demo${path.path}`}className="text-uppercase btn-primary tm-btn mx-auto tm-d-table">Create Place</Link>
+  );
+
     return (
         <div className="tab-pane fade show active" id="2a">
           <h3>South America</h3>
 
-          <Link to={`/demo${path.path}`}className="text-uppercase btn-primary tm-btn mx-auto tm-d-table">Create Place</Link>
-
+            {user
+            ? loggedInUser
+            : null
+            }
               <div className="tm-recommended-place-wrap">
 
                 {places.map(x => <PlaceItem key={x._id} place={x} />)}
