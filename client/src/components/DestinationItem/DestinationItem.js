2203da114346abdc4263299c6f8eb66486bcc44f
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-12-02 15:33:14 +0200

    Create likes logic

--- client/src/components/DestinationItem/DestinationItem.js
+++ client/src/components/DestinationItem/DestinationItem.js
@@ -1,14 +1,33 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import { useState } from 'react';
-
+import * as destinationService from '../Services/data';
 
 const DestinationItem = ({
     destination,
+   
 }) => {
-    
-    const [count, setCount] = useState(0);
 
+
+    const history = useHistory()
+    
+    const { name, description, imgOne, imgTwo, imgThree, likes } = destination;
+
+    const [count, setCount] = useState(likes);
+    
+    let counterLikes = (e) => {
+        e.preventDefault()
+           setCount((count) => count + 1)
+       
+        editLikes()
+     }
+
+     function editLikes(){
+        let data = { name, description, imgOne, imgTwo, imgThree, likes: count };
+        destinationService.edit(destination._id, data)
+        .then(history.push('/destination'))
+     }
+  
 
     return (
         <section className="tm-slideshow-section">
@@ -23,7 +42,7 @@ const DestinationItem = ({
                 <Link to={`/journey/destinations/${destination._id}`} className="text-uppercase tm-btn tm-btn-white tm-btn-white-primary">Continue Reading</Link>
                 <button
                     type='button'
-                    onClick={() => setCount((count) => count + 1)}
+                    onClick={counterLikes}
                 >Likes  {count}</button>
             </div>
         </section>
