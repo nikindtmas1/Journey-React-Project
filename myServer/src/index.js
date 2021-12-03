921657b47329ee3f0224fc9b0e35bae0c8f472a7
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-12-03 22:56:29 +0200

    Install farebase

--- myServer/src/index.js
+++ myServer/src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
+// import { initializeApp } from "firebase/app";
+
 const app = express();
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
@@ -9,6 +11,14 @@ const portConfig = require('./config/config');
 const mongooseConfig = require('./config/configMongoose');
 const routes = require('./routes/routes');
 
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAP6ncGJ9eFKpfmuEn0GIHXME2W46OYyCU",
+//     authDomain: "my-server-journey.firebaseapp.com",
+//     projectId: "my-server-journey",
+//     storageBucket: "my-server-journey.appspot.com",
+//     messagingSenderId: "984395761011",
+//     appId: "1:984395761011:web:44a79177d877ee19e1b106"
+//   };
 // app.use((req, res, next) => {
 
 //     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -24,7 +34,7 @@ const routes = require('./routes/routes');
 
 // });
 
-
+// const app = initializeApp(firebaseConfig);
 app.use(cors());
 
 app.use(routes)
