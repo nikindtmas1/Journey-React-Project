5325f07ddf13e8703900e7ae052ed24bc910913b
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-12-05 21:05:30 +0200

    Added Noplace component

---
+++ client/src/components/Places/PlaceItem/NoPlace.js
@@ -0,0 +1,14 @@
+
+
+const NoPlace = () => {
+     return (
+        <div className="tm-recommended-place">
+        <img src="/img/tm-img-05.jpg" alt="Image" className="img-fluid tm-recommended-img" />
+        <div className="tm-recommended-description-box">
+        <h2 className="text-uppercase tm-banner-title" >No places yet</h2>
+        </div>
+      </div>
+     );
+};
+
+export default NoPlace;
